520221d7c99fc8574c4c11c756de8fecdbb3ac96
LL <2029989979@qq.com>
2024-11-28 17:36:52 +0800

    删除除index.html和index.js外的所有文件

--- dist/parcel-LL.9a9f2c25.js
+++
@@ -1,1 +0,0 @@
-(function () {document.addEventListener("DOMContentLoaded",function(){var e=document.createElement("p");e.textContent="\u540D\u5B57\uFF1A\u674E\u4E50",document.body.appendChild(e)});})();
