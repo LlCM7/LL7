0f776a2463a8524bdf85393947e4a9e6d59407c9
LL <2029989979@qq.com>
2024-11-28 14:08:14 +0800

    提交初始代码

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// index.js
+document.addEventListener('DOMContentLoaded', function() {
+    // 创建一个用于显示文本的元素
+    var textElement = document.createElement('p');
+    // 设置元素的文本内容
+    textElement.textContent = '名字：李乐';
+    // 将元素添加到 body 元素中
+    document.body.appendChild(textElement);
+  });
